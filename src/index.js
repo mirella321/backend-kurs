188ecc8b99c4e943242c2c40fcdb65f977673e4a
Sebastian279333 <kawsebastian@gmail.com>
2026-02-19 16:25:21 +0100

    zmiany

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { title } from "node:process";
 
 
 const app = express();
@@ -7,6 +8,23 @@ const port = 3009;
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get("/api/movies", ()  => {
+
+    const movies= [
+    {
+        title: "Avatar",
+        director : "james cameron"
+    }
+     {
+        title: "Interstellar",
+        director : "XYZ"
+    }
+];
+   res.json(movies);
+}
+)
+ 
+
 
 app.listen(port, () => {
   console.log(`https://localhost:${port} on port ${port}`)
