e5e16ac83e47bb4d63061c11a4eb3fd8800f291f
Sebastian279333 <kawsebastian@gmail.com>
2026-02-20 10:08:33 +0100

    film

--- src/index.js
+++ src/index.js
@@ -1,31 +1,37 @@
 import express from "express";
-import { title } from "node:process";
-
 
 const app = express();
+app.use(express.json());
 const port = 3009;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.get("/api/movies", ()  => {
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-    const movies= [
+app.get("/api/movies", (req, res) => {
+  const movies = [
     {
-        title: "Avatar",
-        director : "james cameron"
+      title: "Avatar",
+      director: "james cameron",
     },
-     {
-        title: "Interstellar",
-        director : "XYZ"
-    }
-];
-   res.json(movies);
+    {
+      title: "Interstellar",
+      director: "XYZ",
+    },
+  ];
+  res.json(movies);
+});
+
+app.post("/api/movies" , (req, res) => 
+{
+  const movie = req.body;
+
+  res.status(201).json({message: "Dodano film", ...movie});
 }
 )
- 
-
 
 app.listen(port, () => {
-  console.log(`https://localhost:${port} on port ${port}`)
-})
+  console.log(`https://localhost:${port} on port ${port}`);
+});
+
+
