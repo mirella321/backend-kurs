df7d64c113695bd167630192893f6fed137ac5ff
Sebastian279333 <kawsebastian@gmail.com>
2026-02-20 13:17:21 +0100

    description

--- src/data/movie.data.js
+++ src/data/movie.data.js
@@ -1,15 +1,17 @@
 
 export const movies = [
-    {
-    id : 1,
-    title : "Avatar",
-    director : "James csmeron",
-    likes:1
- },
-   {
-    id : 2,
-    title : "Avatar",
-    director : "James csmeron",
-    likes:5
- },
+  {
+    id: 1,
+    title: "Avatar",
+    director: "James Cameron",
+    description: "Epicki film science-fiction o konflikcie ludzi z rdzenną rasą Na'vi na planecie Pandora.",
+    likes: 1
+  },
+  {
+    id: 2,
+    title: "Avatar: The Way of Water",
+    director: "James Cameron",
+    description: "Kontynuacja historii Jake’a Sully’ego i Neytiri, skupiająca się na rodzinie oraz nowych zagrożeniach na Pandorze.",
+    likes: 5
+  }
 ];
